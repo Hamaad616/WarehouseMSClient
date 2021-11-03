3be11a73ec47e73c1f26ee6169e4e64d43ff3518
Hamaad Kaleem <46200983+Hamaad616@users.noreply.github.com>
2021-11-03 13:44:46 +0500

    Changes made to web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Middleware\Logger;
 
 /*
 |--------------------------------------------------------------------------
